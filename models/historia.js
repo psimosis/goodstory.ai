63a011c90b86daace09a1557d69d356175cc0147
psimosis <psimosis@gmail.com>
2023-04-26 23:12:05 -0300

    Class Define

---
+++ models/historia.js
@@ -0,0 +1,5 @@
+class historia {
+    constructor() {
+        this.historial = [];
+    }
+}
---
+++ models/usuario.js
@@ -0,0 +1,7 @@
+class usuario {
+    constructor(nombre, apellido, edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+}
