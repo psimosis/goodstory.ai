2d402073465427208c5f044e8c2b513611cb3ad2
psimosis <psimosis@gmail.com>
2023-05-01 23:17:00 -0300

    Specs Tests

--- models/historia.js
+++ models/historia.js
@@ -1,7 +1,11 @@
 class historia {
-    constructor(descripcion, cantpersonajes, genero) {
+    constructor(id, descripcion, cantpersonajes, genero) {
+        this.id = id;
         this.descripcion = descripcion;
         this.cantpersonajes = cantpersonajes;
         this.genero = genero;
     }
 }
+
+
+//export default new historia();
---
+++ test/historia.spec.js
@@ -0,0 +1,18 @@
+let chai = require('chai');
+const { default: historia } = require('../models/historia');
+let expect = chai.expect;
+
+
+describe("Objeto Historia", function(){
+    it("Deberia crear un objeto Historia", function(){
+        let histor = new historia();
+        expect(histor).to.be.an('object');
+    });
+
+    it("Deberia crear un objeto Historia con todos los datos necesarios", function(){
+        let histor = new historia();
+        expect(histor).to.have.all.keys('id', 'descripcion', 'cantpersonajes', 'genero');
+    });
+
+    
+})
