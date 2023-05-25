2a228148198ba811baf81cb888b911f611865135
Lucas <funes.lucas@ttec.com>
2023-05-25 19:04:15 -0300

    Little ABM

--- index.js
+++ index.js
@@ -4,16 +4,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json());
+
+const listaPersonajes = []
+
 app.get('/personajes', (req, res) => {
-  const personaje = new Personaje(1, "Jose", "Heroe", "No es bueno", 22)
-  console.log(personaje)
-  res.send(personaje)
+
+  res.send(listaPersonajes)
 })
 
 app.post('/personaje', (req, res) => {
-    const personaje = new Personaje(1, "Jose", "Heroe", "No es bueno", 22)
-    console.log(personaje)
-    res.send(personaje)
+    const p = req.body
+    listaPersonajes.push(new Personaje(1, p.name, p.tipo, p.descripcion, p.edad))
+    res.send(`Este es el personaje que creaste ${p.name}`)
 })
 
 app.listen(port, () => {
