6871e7732e13798b601b4b51f2400c0ebb896d5d
GuidoComp <guidocompagno@gmail.com>
2023-05-25 20:31:34 -0300

    pull

--- index.js
+++ index.js
@@ -29,28 +29,3 @@ app.listen(port, () => {
 
 app.post('/user', (req, res) => {
     
-    const u = req.body
-    console.log(u);
-    const usuarioCreado = new Usuario(u.nombre, u.apellido, u.username, u.password);
-    listaUsuarios.push(usuarioCreado)
-    res.send(`Hola ${usuarioCreado.nombre}, creamos tu usuario ${usuarioCreado.username}.`)
-})
-
-// app.get('/user/{id}', (req, res) => {
-//     const u = req.body
-//     listaUsuarios.push(new Usuario(1, p.name, p.tipo, p.descripcion, p.edad))
-//     res.send(`Este es el personaje que creaste ${p.name}`)
-// })
-
-app.get('/login', (req, res) => {
-    const user = req.body
-
-    //Hacer validaciones de usuario/pass
-
-    const tokgen2 = new TokenGenerator(256, TokenGenerator.BASE62);
-    const token = tokgen2.generate();
-
-    // Agregar token al usuario 
-
-    res.send(`Hola ${req.body.username} tu token es ${token}`)
-})
